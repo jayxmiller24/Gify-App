380eb370b5589702b59d0a13097458691590495f
jxmiller5 <jxmiller5@gmail.com>
2019-03-17 08:47:38 -0700

    removed some comments

--- assets/javascript.js
+++ assets/javascript.js
@@ -56,40 +56,36 @@ $(document).ready(function () {
 
 
 
-    // Function for displaying movie data
     function renderButtons() {
 
-        // Deleting the movies prior to adding new movies
-        // (this is necessary otherwise you will have repeat buttons)
+        
         $("#buttons-view").empty();
 
-        // Looping through the array of movies
+        
         for (var i = 0; i < animalArray.length; i++) {
 
-            // Then dynamicaly generating buttons for each movie in the array
-            // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+            
             var a = $("<button>");
-            // Adding a class of movie-btn to our button
+            
             a.addClass("animal-btn");
-            // Adding a data-attribute
+            
             a.attr("data-name", animalArray[i]);
-            // Providing the initial button text
             a.text(animalArray[i]);
-            // Adding the button to the buttons-view div
+            
             $("#buttons-view").append(a);
         }
     }
 
-    // This function handles events where a movie button is clicked
+    
     $("#add-animal").on("click", function (event) {
         event.preventDefault();
         // This line grabs the input from the textbox
         var animals = $("#animal-input").val().trim();
 
-        // Adding movie from the textbox to our array
+        
         animalArray.push(animals);
 
-        // Calling renderButtons which handles the processing of our movie array
+        
         renderButtons();
     });
 
